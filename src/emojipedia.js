2ac825f2d6144a3f6315a627228787c205d6f9aa
KMONJARD <62231623+kmonjard@users.noreply.github.com>
2021-04-26 17:18:50 -0700

    Initial commit

---
+++ src/emojipedia.js
@@ -0,0 +1,88 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "🤓",
+    name: "Nerd Face",
+    meaning:
+      "Huge glasses, awkward smile and buck teeth. Used humorously or ironically for nerds or to express how smart you are. Stereotype of a nerd; a smart but funny-dressed person with social deficits."
+  },
+  {
+    id: 5,
+    emoji: "🐨",
+    name: "Koala",
+    meaning:
+      "The koala stands for cuteness or amiability and awakens protective instincts. The second best-known animal in Australia feeds on eucalyptus and sleeps 20 hours a day."
+  },
+  {
+    id: 6,
+    emoji: "🦁",
+    name: "Lion Face",
+    meaning:
+      "A cute lion face, the king of animals and the jungle. A powerful, dangerous predator, a sublime ruler over its pack and a symbol of power."
+  },
+  {
+    id: 7,
+    emoji: "🐯",
+    name: "Tiger Face",
+    meaning:
+      "The magnificent animal stands for strength, felinity and appetite. Central importance in Asia. There, the tiger is considered a wise leader (Buddhism) and national animal (India), it is a symbol of bravery and strength (China) and a Chinese zodiac."
+  },
+  {
+    id: 8,
+    emoji: "🍄",
+    name: "Mushroom",
+    meaning:
+      "Let’s go on a mushroom foray! To spring up like mushrooms! An extra life with Super Mario. The mushroom with the red umbrella and the white dots is the poisonous fly agaric. Can refer to fall as well as to a healthy diet."
+  },
+  {
+    id: 9,
+    emoji: "🌻",
+    name: "Sunflower",
+    meaning:
+      "You make the sun rise! The “high spirits flower“ conjures a smile on every face. Represents joie de vivre, happiness and warmth. Sunflowers are a symbol of the summer."
+  },
+  {
+    id: 10,
+    emoji: "🌎",
+    name: "Globe with America",
+    meaning:
+      "Our next destination is America! View from space of the continent of America with North and South America. Refers to the continent, a single country or the earth in general."
+  },
+  {
+    id: 11,
+    emoji: "🌙",
+    name: "Crescent Moon",
+    meaning:
+      "I wish you a good night! The moon forms a crescent in the night sky. This may indicate the beginning or the end of a journey."
+  },
+  {
+    id: 12,
+    emoji: "🪐",
+    name: "Ringed planet",
+    meaning:
+      "The solar system fascinates me! The planet with a ring is Saturn, the second largest planet. The gas planet is visible to the naked eye. Symbolic for reliability and stability."
+  }
+];
+
+export default emojipedia;
